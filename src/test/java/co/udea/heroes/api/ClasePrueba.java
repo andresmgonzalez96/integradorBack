6cf7930f8227312059549cff0f37b085dbe27b54
Andres Gonzalez <agonzalezr@psl.com.co>
2019-10-15 15:22:12 -0500

    suma fail

--- src/test/java/co/udea/heroes/api/ClasePrueba.java
+++ src/test/java/co/udea/heroes/api/ClasePrueba.java
@@ -8,8 +8,16 @@ import org.junit.Test;
 public class ClasePrueba {
 	
 	@Test
-	public void loginExitoso() {
+	public void testSuma() {
 		int suma = 3 + 2;
+		
+		Assert.assertEquals(5, suma);
+		
+	}
+	
+	@Test
+	public void testSumaError() {
+		int suma = 3 + 3;
 		
 		Assert.assertEquals(5, suma);
 		
