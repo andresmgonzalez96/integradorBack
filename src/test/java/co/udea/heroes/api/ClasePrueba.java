21af9a3195be813477b891f064a3dc225b0c392f
Andres Gonzalez <agonzalezr@psl.com.co>
2019-10-15 16:09:18 -0500

    suma fail

--- src/test/java/co/udea/heroes/api/ClasePrueba.java
+++ src/test/java/co/udea/heroes/api/ClasePrueba.java
@@ -2,9 +2,13 @@ package co.udea.heroes.api;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ClasePrueba {
 	
 	@Test
