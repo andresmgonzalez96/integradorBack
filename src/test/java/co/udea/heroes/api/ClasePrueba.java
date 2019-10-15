e6f6861a7d70f7319f9e8cd3220039ee0d3aef42
Andres Gonzalez <agonzalezr@psl.com.co>
2019-10-15 15:16:35 -0500

    Test

---
+++ src/test/java/co/udea/heroes/api/ClasePrueba.java
@@ -0,0 +1,19 @@
+package co.udea.heroes.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class ClasePrueba {
+	
+	@Test
+	public void loginExitoso() {
+		int suma = 3 + 2;
+		
+		Assert.assertEquals(5, suma);
+		
+	}
+	
+
+}
